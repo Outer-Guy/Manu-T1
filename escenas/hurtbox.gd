81d71845574d2dd5ae20b4c10cd652ad6309718a
manuel <manuelkaplan2020@gmail.com>
2026-05-14 18:53:36 -0300

    inicio daño

---
+++ escenas/hurtbox.gd
@@ -0,0 +1,10 @@
+extends Area3D
+
+func _on_body_entered(body: Node3D) -> void:
+	pass # Replace with function body.
+
+var ListaIgnorar : Array [Node3D]
+
+var vida = 100 
+
+var vida = -20
---
+++ scrips/armas/arma_resorse.gd
@@ -0,0 +1,5 @@
+class_name armaresourse
+extends Resource
+
+@export var daño : int
+@export var nombre : String 
