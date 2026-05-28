5f979c26212c9e35f1d9edb1da86a6f103fc6392
manuel <manuelkaplan2020@gmail.com>
2026-05-27 23:07:56 -0300

    arrreglar bugs (attack, movimiento)

--- escenas/hurtbox.gd
+++ escenas/hurtbox.gd
@@ -8,18 +8,18 @@ const hurtboxScene : PackedScene = preload("res://escenas/hurtbox.tscn")
 
 static func _new_hurt_box(new_position : Vector3, body: Node3D, new_lifetime : float) -> Node3D:
 	var new_hurtbox : Node3D = hurtboxScene.instantiate()
-sd	new_hurtbox.ListaIgnorar.apend(body)
+	new_hurtbox.ListaIgnorar.apend(body)
 	new_hurtbox.position = new_position
 	new_hurtbox.lifetime = new_lifetime
 	
 	print("spawn: ", new_position)
 	return new_hurtbox
 	
-	func _psysics_process(delta: float) -> void:
+func _psysics_process(delta: float) -> void:
 		lifetime -= delta
 		if lifetime <= 0:
-		queve_free()
-
+			queue_free() 
+		
 func _on_body_entered(body: Node3D) -> void:
 	for node in ListaIgnorar:
 		if(node == body):
