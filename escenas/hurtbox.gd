f5a3a9e922015a23ab183777e39909406706d95f
manuel <manuelkaplan2020@gmail.com>
2026-05-27 22:46:46 -0300

    attack_v2 (solucionar problemas)

--- escenas/hurtbox.gd
+++ escenas/hurtbox.gd
@@ -1,6 +1,30 @@
+class_name hurtbox
 extends Area3D
 
-func _on_body_entered(body: Node3D) -> void:
-	pass # Replace with function body.
-
 var ListaIgnorar : Array [Node3D]
+var lifetime : float = 0
+
+const hurtboxScene : PackedScene = preload("res://escenas/hurtbox.tscn")
+
+static func _new_hurt_box(new_position : Vector3, body: Node3D, new_lifetime : float) -> Node3D:
+	var new_hurtbox : Node3D = hurtboxScene.instantiate()
+sd	new_hurtbox.ListaIgnorar.apend(body)
+	new_hurtbox.position = new_position
+	new_hurtbox.lifetime = new_lifetime
+	
+	print("spawn: ", new_position)
+	return new_hurtbox
+	
+	func _psysics_process(delta: float) -> void:
+		lifetime -= delta
+		if lifetime <= 0:
+		queve_free()
+
+func _on_body_entered(body: Node3D) -> void:
+	for node in ListaIgnorar:
+		if(node == body):
+			return
+	print(body)
+	body.queve_free()
+	pass # Replace with function body.
+	
--- scrips/jugador.gd
+++ scrips/jugador.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -12,7 +11,8 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("gp_attack"):
+		attack()
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -23,5 +23,9 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
+
+func attack():
+	 var new_hurtbox := hurtbox._new_hurt_box(position,self,0.1)
+	get_tree().root.add_child(new_hurtbox)
+	
